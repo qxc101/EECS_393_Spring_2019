04becc68c4d97e81095f34ba0b61edb323aa425c
Melo <31557544+fatcha14@users.noreply.github.com>
2019-03-18 01:57:35 -0400

    add 3 fragments to the drawer menu
    Signed-off-by: Melo <31557544+fatcha14@users.noreply.github.com>

--- app/src/main/java/com/example/quickoff/MainActivity.java
+++ app/src/main/java/com/example/quickoff/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.quickoff;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -19,11 +22,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home_button);
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.home_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.preferred_list_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PreferredListFragment()).commit();
+                break;
+            case R.id.contact_us_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactUsFragment()).commit();
+                break;
+        }
+        return true;
     }
 
     @Override
