b2573feb788f178061ed734d4bc8d50469640326
Rongjie Zeng <fatcha14@gmail.com>
2019-03-19 14:53:08 -0400

    add search result activity
    Signed-off-by: Rongjie Zeng <fatcha14@gmail.com>

--- app/src/main/java/com/example/quickoff/MainActivity.java
+++ app/src/main/java/com/example/quickoff/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.quickoff;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -38,6 +40,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    public void searchClicked(View view) {
+        Intent startSearchResult = new Intent(this, SearchResultActivity.class);
+        startActivity(startSearchResult);
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
