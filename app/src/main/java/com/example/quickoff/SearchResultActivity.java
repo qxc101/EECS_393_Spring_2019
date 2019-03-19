bb2d11c97a0ea3deeb3e49df93f40f4396ed016d
Rongjie Zeng <fatcha14@gmail.com>
2019-03-19 15:05:29 -0400

    add back btn to the search result page
    Signed-off-by: Rongjie Zeng <fatcha14@gmail.com>

--- app/src/main/java/com/example/quickoff/SearchResultActivity.java
+++ app/src/main/java/com/example/quickoff/SearchResultActivity.java
@@ -1,5 +1,6 @@
 package com.example.quickoff;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Search Result");
+
     }
 }
